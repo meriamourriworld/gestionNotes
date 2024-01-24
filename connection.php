2c35e40dbf62728b25729b92c1da6c4e2f1380e9
Meriam Ourri <ourrimeriam@gmail.com>
2024-01-25 00:32:45 +0100

    Added login page design

---
+++ connection.php
@@ -0,0 +1,55 @@
+<?php 
+    $servername = "localhost";
+    $db = "gestionnotes";
+    $username = "root";
+    $pass = "";
+
+    try
+    {
+        $con = new PDO("mysql:host=$servername; dbname=$db;", $username, $pass);
+        $con->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+
+
+
+
+    }catch(PDOEXCEPTION $e)
+    {
+        $error = "Une erreur est survenue " . $e->getMessage();
+        header("location:errorPage.php?msg=$error");
+    }
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Connectez-vous</title>
+</head>
+<body class="container-100">
+
+    <main class="mainConnection mx-auto d-flex justify-content-around align-items-center">
+        <div class="wallLogin"></div>
+        <div class="formLogin d-flex flex-column justify-content-center align-items-center text-white">
+            <div class="logo text-center">
+                <h1 class="display-4">Madrasati</h1>
+                <h6>Votre succès est Notre affaire</h6>
+            </div>
+            <form class="mt-5 w-100" action="connection.php" method="post">
+                <label for="login" class="form-label mt-4">Identifiant :</label>
+                <input type="text" class="form-control p-3" name="login" id="login">
+
+                <label for="pass" class="form-label mt-4">Mot de passe :</label>
+                <input type="password" class="form-control p-3" name="pass" id="pass">
+
+                <input type="submit" value="Se connecter" class="btn btn-reg w-100 mt-4 p-3 text-white">
+            </form>
+        </div>
+    </main>
+</body>
+</html>
---
+++ errorPage.php
@@ -0,0 +1,32 @@
+<?php 
+
+    $errMsg = "ERROR";
+    if(isset($_GET["msg"]))
+    {
+        $errMsg = $_GET["msg"];
+    }
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="./styles/style.css">
+    <title>Error</title>
+</head>
+<body>
+    <main class="mainErrPage">
+        <section class="errorContainer text-center">
+            <h1 class="display-1">Désolé !</h1>
+            <h3>Le problème sera résolu dans les plus brefs délais !</h3>
+            <p class="pt-5 font-weight-bold"><?php echo $errMsg; ?></p>
+        </section>
+
+    </main>
+
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
